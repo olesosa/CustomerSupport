98e2f78286afbb6c316480272f64b2cd0b86392d
olesosa <oleosad@gmail.com>
2024-03-19 14:17:07 +0200

    updated dto models

--- CS.DOM/Helpers/ApiException.cs
+++ CS.DOM/Helpers/ApiException.cs
@@ -4,4 +4,15 @@ public class ApiException : Exception
 {
     public int Status { get; set; }
     public string Detail { get; set; }
+
+    
+    public ApiException(int status, string msg) : base(msg)
+    {
+        Status = status;
+    }
+    public ApiException(int status, string msg, string detail) : base(msg)
+    {
+        Status = status;
+        Detail = detail;
+    }
 }
