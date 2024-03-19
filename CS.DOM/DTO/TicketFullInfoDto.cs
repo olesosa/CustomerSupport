80eb9a7243acb4fff1798836c1076918c78971a9
olesosa <oleosad@gmail.com>
2024-03-19 14:17:07 +0200

    updated dto models

--- CS.DOM/DTO/TicketFullInfoDto.cs
+++ CS.DOM/DTO/TicketFullInfoDto.cs
@@ -4,13 +4,13 @@
     {
         public Guid Id { get; set; }
         public Guid CustomerId { get; set; }
+        public int Number { get; set; }
         public string RequestType { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
         public bool IsAssigned { get; set; }
         public bool IsSolved { get; set; }
         public bool IsClosed { get; set; }
-        public string Topic { get; set; }
-        public string Description { get; set; }
-        public List<string> AttachmentsFilePath { get; set; }
         public DateTime? CreationTime { get; set; }
     }
 }
--- CS.DOM/DTO/UserDto.cs
+++ CS.DOM/DTO/UserDto.cs
@@ -2,5 +2,6 @@ namespace CS.DOM.DTO;
 
 public class UserDto
 {
-    
+    public Guid Id { get; set; }
+    public string Email {get; set;}
 }
